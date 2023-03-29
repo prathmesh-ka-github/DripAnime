1dfb75f31a52a1cc32cb3e05ea52e49a5ea797ca
Prathmesh Kale <connect.prathmesh905@gmail.com>
2023-03-29 21:06:28 +0530

    added json and started work on backend and products

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const ham = document.getElementById('ham');
 const hamcontents = document.getElementById('ham-contents');
-// for (i=0; i<hamcontents.length; i++) {
-//     ham.addEventListener('click', function () {
-//         hamcontents.classList.toggle('active')
-//         console.log('clicked')
-//     })
-// }
 ham.addEventListener('click', function () {
     console.log('clicked!')
     hamcontents.classList.toggle('active')
 })
+
+
+fetch('./tshirt.json')
+    .then((response) => response.json())
+    .then((data) => console.log(data.hoodie[0].id));
