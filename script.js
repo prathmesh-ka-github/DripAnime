1881d859b49b1f2d41ee7bbc8633b12561ace641
Prathmesh Kale <connect.prathmesh905@gmail.com>
2023-03-30 14:21:50 +0530

    added hoodie json to search test

--- script.js
+++ script.js
@@ -15,7 +15,12 @@ searchInput.addEventListener("input", (e) => {
     users.forEach(user => {
         const isVisible = user.name.toLowerCase().includes(value) || user.tags.toLowerCase().includes(value)
         user.element.classList.toggle("hide",!isVisible)
-        console.log(user.tags)
+        // console.log(user.tags)
+    })
+    husers.forEach(huser => {
+        const isVisible = huser.hname.toLowerCase().includes(value) || huser.htags.toLowerCase().includes(value)
+        huser.helement.classList.toggle("hide",!isVisible)
+        // console.log(huser.htags)
     })
 })
 
@@ -24,12 +29,27 @@ fetch('./tshirt.json')
     .then(data => {
         users = data.tshirt.map(product => {
             const card = userCardTemplate.content.cloneNode(true).children[0]
-            console.log(product)
+            // console.log(product)
             const header = card.querySelector('[data-name]')
             const tags = card.querySelector('[data-tags]')
             header.textContent = product.name
             tags.textContent = product.tags
             searchResult.append(card)
             return{name:product.name, tags:product.tags, element:card}
+        });
+    });
+
+fetch('./tshirt.json')
+    .then((hresponse) => hresponse.json())
+    .then(hdata => {
+        husers = hdata.hoodie.map(hoodie => {
+            const card = userCardTemplate.content.cloneNode(true).children[0]
+            // console.log(hoodie)
+            const header = card.querySelector('[data-name]')
+            const tags = card.querySelector('[data-tags]')
+            header.textContent = hoodie.name
+            tags.textContent = hoodie.tags
+            searchResult.append(card)
+            return{hname:hoodie.name, htags:hoodie.tags, helement:card}
         });
     });
