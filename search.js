976b1249a17834b64e7e8d16f37f34e0542a419c
Prathmesh Kale <connect.prathmesh905@gmail.com>
2023-04-01 23:33:31 +0530

    added imgs and cloning of templates working

--- search.js
+++ search.js
@@ -1,29 +1,33 @@
 /*
     This is the beta testing stage for now with dummy text.
-
     TODO Add img urls and make working cards of products.
+    TODO Now adding products
 */
 // For search-bar
 const userCardTemplate = document.querySelector("[data-user-template]")
 const searchResult = document.querySelector("[search-results]")
 const searchInput = document.querySelector("[data-search]")
+console.log(searchInput)
 
 let users = []
-let husers = []
 
 //! Searching and displaying the search results
 searchInput.addEventListener("input", (e) => {
     const value = e.target.value.toLowerCase()
     // for tshirts
-    users.forEach(user => {
-        const isVisible = user.name.toLowerCase().includes(value) || user.tags.toLowerCase().includes(value)
-        user.element.classList.toggle("hide",!isVisible)
-    })
-    // for hoodies
-    husers.forEach(huser => {
-        const isVisible = huser.hname.toLowerCase().includes(value) || huser.htags.toLowerCase().includes(value)
-        huser.helement.classList.toggle("hide",!isVisible)
-    })
+    if (value !== "") {
+        users.forEach(user => {
+            const isVisible = user.name.toLowerCase().includes(value) || user.tags.toLowerCase().includes(value)
+            user.element.classList.toggle("hide",!isVisible)
+            // document.querySelector("[search-results]").classList.toggle("hide",!isVisible)
+        })
+    }
+    else if(value == ""){
+        users.forEach(user => {
+            user.element.classList.toggle("hide")
+        })
+        // document.querySelector("[search-results]").classList.toggle("hide")
+    }
 })
 
 //! Inserting items into DOM using tshirt.JSON file
@@ -34,27 +38,27 @@ fetch('./products/data/tshirt.json')
     users = data.tshirt.map(product => {
             const card = userCardTemplate.content.cloneNode(true).children[0]
             // console.log(product)
+            const productImage = card.querySelector("[product-image]")
             const header = card.querySelector('[data-name]')
-            const tags = card.querySelector('[data-tags]')
+            const imgurl = product.url
+            productImage.innerHTML = `<img src="products/images/${imgurl}.png" alt="${imgurl}">`
             header.textContent = product.name
-            tags.textContent = product.tags
             searchResult.append(card)
             return{name:product.name, tags:product.tags, element:card}
         });
     });
     
     // for hoodies
-fetch('./products/data/tshirt.json')
-    .then((hresponse) => hresponse.json())
-    .then(hdata => {
-        husers = hdata.hoodie.map(hoodie => {
-            const card = userCardTemplate.content.cloneNode(true).children[0]
-            // console.log(hoodie)
-            const header = card.querySelector('[data-name]')
-            const tags = card.querySelector('[data-tags]')
-            header.textContent = hoodie.name
-            tags.textContent = hoodie.tags
-            searchResult.append(card)
-            return{hname:hoodie.name, htags:hoodie.tags, helement:card}
-        });
-    });
+// fetch('./products/data/tshirt.json')
+//     .then((hresponse) => hresponse.json())
+//     .then(hdata => {
+//         husers = hdata.hoodie.map(hoodie => {
+//             const card = userCardTemplate.content.cloneNode(true).children[0]
+//             // console.log(hoodie)
+//             const header = card.querySelector('[data-name]')
+//             const tags = card.querySelector('[data-tags]')
+//             header.textContent = hoodie.name
+//             searchResult.append(card)
+//             return{hname:hoodie.name, htags:hoodie.tags, helement:card}
+//         });
+//     });
