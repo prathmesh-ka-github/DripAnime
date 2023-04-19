df2300e486a19f11bd8c765bdd9b05289609ad08
Prathmesh Kale <connect.prathmesh905@gmail.com>
2023-04-19 21:21:42 +0530

    added express.static in server.js

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+const path = require('path')
 app.use(express.json())
 
 const users = [];
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/',(req, res) => {
+    // res.writeHead(200,{ 'Content-Type':'html' })
+    res.sendFile( __dirname + '/index.html')
+})
 
 app.get('/users', (req,res) => {
     res.json(users);
 })
 
-app.listen(3000)
+app.listen(port,() => {
+    console.log(`Listening to http://localhost:${port}/`)
+})
