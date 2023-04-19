1b5fc2f284f346872c967bb872587b9ee7cedacc
Prathmesh Kale <connect.prathmesh905@gmail.com>
2023-04-19 15:48:39 +0530

    moved product pages and search engines

--- products/search-engines/product-hoodie.js
+++ products/search-engines/product-hoodie.js
@@ -5,15 +5,15 @@ let users = []
 
 //! Inserting items into DOM using products.JSON file
 // for tshirts
-fetch('./products/data/products.json')
+fetch('./data/products.json')
 .then((response) => response.json())
 .then(data => {
-    users = data.tshirt.map(product => {
+    users = data.hoodie.map(product => {
         const card = userCardTemplate.content.cloneNode(true).children[0]
         const productImage = card.querySelector("[product-image]")
         const header = card.querySelector('[data-name]')
         const imgurl = product.url
-        productImage.innerHTML = `<img src="products/images/${imgurl}.png" alt="${imgurl}">`
+        productImage.innerHTML = `<img src="images/${imgurl}.png" alt="${imgurl}">`
         header.textContent = product.name
         searchResult.append(card)
         return{name:product.name, tags:product.tags, element:card}
