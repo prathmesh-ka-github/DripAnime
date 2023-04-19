1e2ff58c3a124481b67c27555ff634a5c090204f
Prathmesh Kale <connect.prathmesh905@gmail.com>
2023-04-19 21:38:49 +0530

    adding routes to index.html

--- server.js
+++ server.js
@@ -1,22 +1,58 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = require('path')
-app.use(express.json())
 
+app.use(express.json())
 const users = [];
 
 app.use(express.static(__dirname + '/'));
 
+
+//! ALL GET ENDPOINTS!!!
 app.get('/',(req, res) => {
     // res.writeHead(200,{ 'Content-Type':'html' })
+    res.status(200)
     res.sendFile( __dirname + '/index.html')
+})
+
+app.get('/home', (req,res) => {
+    res.status(200)
+    res.sendFile( __dirname + '/index.html')
+})
+
+app.get('/apparels', (req,res) => {
+    res.status(200)
+    res.sendFile( __dirname + '/apparels.html')
+})
+
+app.get('/anime', (req,res) => {
+    res.status(200)
+    res.sendFile( __dirname + '/anime.html')
+})
+
+app.get('/support', (req,res) => {
+    res.status(200)
+    res.sendFile( __dirname + '/support.html')
+})
+app.get('/search', (req,res) => {
+    res.status(200)
+    res.sendFile( __dirname + '/search.html')
+})
+app.get('/register', (req,res) => {
+    res.status(200)
+    res.sendFile( __dirname + '/register.html')
 })
 
 app.get('/users', (req,res) => {
     res.json(users);
 })
 
+
+
+
+
+
+//! LISTEN
 app.listen(port,() => {
     console.log(`Listening to http://localhost:${port}/`)
 })
