8ef3d6ee74dac386b3490506fc862b4a60bd19a0
Prathmesh Kale <connect.prathmesh905@gmail.com>
2024-02-01 19:05:27 +0530

    fixes in endpoints and pre-loader minor fixes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ ham.addEventListener('click', function () {
     hamcontents.classList.toggle('active')
 })
 
-
+// pre-loader script
 const loader = document.querySelector(".loader-container-main")
 window.addEventListener("load",function(){
     setTimeout(() => {
