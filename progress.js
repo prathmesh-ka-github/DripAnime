b936db1fce8b59f7ed1e312391bda6efe8461c37
Prathmesh Kale <connect.prathmesh905@gmail.com>
2024-02-06 22:22:36 +0530

    created progress.js and made all the current documentation

---
+++ progress.js
@@ -0,0 +1,27 @@
+/* 
+? ---------------------------------------
+? Used to track progress on this project.
+? ---------------------------------------
+
+!1) Make a fully functional login Signup system.
+    > Make a signup system working.
+        Host a DB.
+        Connect the backend to a DB.
+        Add new customers to DB upon submitting the signup form.
+    
+    > Make a login system working. (hard part)
+        Get login data from the form.
+        Sync with DB for customer info.
+        figure out how to keep user logged in.
+
+!2) Take out all Products from experimental stage to production ready stage.
+    > Upload product data to a seperate DB.
+        Host a product DB.
+        Upload all the required info on the hosted DB.
+    
+    > Real time rendering of products in the catalog.
+        Get info for products from the DB.
+        Render the products in catalog.
+
+
+*/
