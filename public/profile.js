255d6e5f6e45395c8fe8bb53838ae7b3bc46cf12
prathmesh-ka-github <pkale@albany.edu>
2025-06-20 19:28:40 -0400

    added profile edit section. Frontend done. DB config and api due.

--- public/profile.js
+++ public/profile.js
@@ -6,6 +6,10 @@ async function getUserDetails() {
     const phone = document.getElementById('phone')
     const useremail = localStorage.getItem('userEmail')
 
+    const edName = document.getElementById('ed-name')
+    const edEmail = document.getElementById('ed-email')
+    const edAddress = document.getElementById('ed-address')
+    const edPhone = document.getElementById('ed-phone')
     await fetch('/getuserdetails', {
         method: 'POST',
         headers: {
@@ -19,6 +23,11 @@ async function getUserDetails() {
             email.innerHTML = data.email
             phone.innerHTML = "+1 " + data.phonenumber
             address.innerHTML = data.address
+
+            edName.placeholder = data.username
+            edEmail.placeholder = data.email
+            edPhone.placeholder = "+1 " + data.phonenumber
+            edAddress.placeholder = data.address
         })
     })
 }
@@ -32,4 +41,14 @@ if (localStorage.getItem('userEmail')) {
 function logout() {
     localStorage.removeItem("userEmail");
     window.location.href="/login"
+}
+
+function showedit() {
+    const editpage = document.getElementById('edit-page')
+    const profilepage = document.getElementById('profile-page')
+    editpage.style.display = 'flex'
+    profilepage.style.display = 'none'
+}
+function showprofile() {
+    window.location.href="/profile"
 }
